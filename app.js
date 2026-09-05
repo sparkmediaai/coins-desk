25b42e115c1f43ce06dbe8608c1ccd564aa3a11e
sparkmediaai <sparkmediaai@gmail.com>
2026-09-04 21:57:51 -0700

    pages: BOOK shows leftover USD when all-SOL (no open clips)

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-PLACEHOLDER_WILL_REPLACE
+/* bootstrap: restore app + BOOK leftover USD (all-SOL) */
+(function(){
+  var u="https://cdn.jsdelivr.net/gh/sparkmediaai/coins-desk@0987bf590ae9cd1671c99276791a18f94887c28a/app.js";
+  var x=new XMLHttpRequest();
+  x.open("GET",u,false);
+  x.send(null);
+  if(x.status<200||x.status>=300) throw new Error("app bootstrap "+x.status);
+  var t=x.responseText;
+  t=t.replace("d=r&&s!=null?o+s:r&&i===0?o:null","d=s!=null?(r?o:0)+s:r?o:null");
+  if(t.indexOf("d=s!=null?(r?o:0)+s:r?o:null")<0) throw new Error("app bootstrap patch miss");
+  (0,eval)(t);
+})();
